accaeb85a2836c0023a0438d1215508df692ed52
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:19:53 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make -j $SHED_NUMJOBS -f unix/Makefile generic && \
-make prefix="${SHED_FAKEROOT}/usr" \
+make -j $SHED_NUM_JOBS -f unix/Makefile generic && \
+make prefix="${SHED_FAKE_ROOT}/usr" \
      MANDIR=/usr/share/man/man1 \
      -f unix/Makefile install
